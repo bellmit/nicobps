e6bf3c5e8c47b53a81ff4cf1b50e8efd3c9e1736
Avijit <Avijit@DESKTOP-Q9V05SL>
2021-05-19 21:43:47 +0530

    test commit

--- obps/src/obps/util/common/Utilty.java
+++ obps/src/obps/util/common/Utilty.java
@@ -3,8 +3,6 @@ package obps.util.common;
 public class Utilty 
 {
 	public static int getRandomNumber() {
-	    //Random r = new Random( System.currentTimeMillis() );
-	    //return ((1 + r.nextInt(2)) * 10000 + r.nextInt(10000));
         String num =Math.random()+"";
         return Integer.valueOf(num.substring(2,8));		
 	}
