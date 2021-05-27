c786e120943ca29ad90639c57b4ecdf68e833afa
Decent Khongstia <decentkhongstia@gmail.com>
2021-05-27 15:42:08 +0530

    BPA-Adding JS file

---
+++ obps/WebContent/resources/js/application/bpa/buildingpermitsteptwo.js
@@ -0,0 +1,16 @@
+/**
+ * @author Decent Khongstia
+ */
+
+app.controller("CommonCtrl", [
+  "$scope",
+  "$http",
+  "$timeout",
+  "$compile",
+  "$window",
+  "commonInitService",
+  "bpaService",
+  function ($scope, $http, $timeout, $compile, $window, CIS, BS) {
+	
+	}
+]);
