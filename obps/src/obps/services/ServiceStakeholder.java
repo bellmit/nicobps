27ce5584d951216d5b5824650ee8b64bb52aec8c
kaushik <kdebnath56@gmail.com>
2022-02-25 12:26:22 +0530

    stakeholder listlicense bugfix

--- obps/src/obps/services/ServiceStakeholder.java
+++ obps/src/obps/services/ServiceStakeholder.java
@@ -31,7 +31,7 @@ public class ServiceStakeholder implements ServiceStakeholderInterface {
 				+ " FROM nicobps.licensees l "
 				+ "INNER JOIN masters.licenseetypes lt on lt.licenseetypecode=l.licenseetypecode "
 				+ "INNER JOIN masters.districts d on d.districtcode=l.predistrictcode "
-				+ "INNER JOIN nicobps.applications app on app.usercode=l.usercode "
+				+ "INNER JOIN nicobps.applications app on app.usercode=l.usercode and app.modulecode=1 "
 				+ "INNER JOIN nicobps.applicationflowremarks afr on  "
 				+ "		afr.afrcode=(select max(afrcode) from nicobps.applicationflowremarks where applicationcode=app.applicationcode::text) "
 				+ "INNER JOIN masters.processflow pf on afr.toprocesscode=pf.fromprocesscode and pf.processflowstatus='N' and pf.modulecode=1 "
